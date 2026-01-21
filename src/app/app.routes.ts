fa9a35211e6e0fac25d6b31239d6c0d512159eee
maneeshkumar <maneeshk@unicourt.com>
2026-01-21 12:26:16 +0530

    feat:Added Observable Concept

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { DataBidingConcept } from './concepts/data-biding-concept/data-biding-concept';
 import { DirectivesComponent } from './concepts/directive-concept/directive.component';
 import { ParentComponent } from './concepts/component-communication-concept/parent/parent.component';
+import { ObservableComponent } from './concepts/observable-concept/observable.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     {path: 'data-binding',component:DataBidingConcept},
     {path:'directives', component:DirectivesComponent},
-    {path:'component-communication',component:ParentComponent}
+    {path:'component-communication',component:ParentComponent},
+    {path:'observable',component:ObservableComponent},
 ];
